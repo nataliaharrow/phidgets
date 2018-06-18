2a71697e79b24b6558ccf9619b237cfb756a8e8b
natharrow <nataliaharrow@gmail.com>
2018-06-18 11:47:21 -0400

    Update Spatial.py

--- Spatial.py
+++ Spatial.py
@@ -16,6 +16,7 @@ except RuntimeError as e:
 def SpatialAttached(e):
     try:
         attached = e
+        e.setCompassCorrectionParameters(self, 0.36341, 0.09927, 0.11774, -0.07688,  2.66012, 2.56018, 3.03476, 0.01156, -0.07656, 0.00600, 0.06844, -0.05628, 0.07447)
         print("\nAttach Event Detected (Information Below)")
         print("===========================================")
         print("Library Version: %s" % attached.getLibraryVersion())
@@ -34,7 +35,7 @@ def SpatialAttached(e):
         print("Press Enter to Exit...\n")
         readin = sys.stdin.read(1)
         exit(1)   
-    
+
 def SpatialDetached(e):
     detached = e
     try:
@@ -55,12 +56,13 @@ This event is fired at a fixed rate as determined by the DataRate property.
 """
 def SpatialDataHandler(e, acceleration, angularRate, fieldStrength, timestamp):
 #write the data into a file
-    file = open("northSteady.txt", "a")
+    
+    file = open("calibrated.txt", "a")
     file.write("Field Strength: %7.3f  %8.3f  %8.3f \n" % (fieldStrength[0], fieldStrength[1], fieldStrength[2]))
-    file.write("\n")
+    #file.write("\n")
 
-    filePlot = open("plotNorthSteadyX.txt", "a")
-    filePlot.write("%7.3f" % (fieldStrength[0]))
+   # filePlot = open("plotNorthSteadyX.txt", "a")
+   # filePlot.write("%7.3f" % (fieldStrength[0]))
 
     print("Field Strength: %7.3f  %8.3f  %8.3f" % (fieldStrength[0], fieldStrength[1], fieldStrength[2]))
 
@@ -80,11 +82,11 @@ try:
     print("Waiting for the Phidget Spatial Object to be attached...")
  #line to clea the file:   open("northToWest.txt", 'w').close()
 
-    file = open("northSteady.txt", "a")
+    file = open("calibrated.txt", "a")
     file.write("--------------------------------------------")
-    file.write("\n")
+    #file.write("\n")
 
-    filePlot = open("plotNorthSteadyX.txt", "a")
+    #filePlot = open("plotNorthSteadyX.txt", "a")
 
     ch.openWaitForAttachment(5000)
 
@@ -106,48 +108,4 @@ except PhidgetException as e:
     exit(1) 
 print("Closed Spatial device")
 exit(0)
-
-
-
-
-
-
-    # Please review the Phidget22 channel matching documentation for details on the device
-    # and class architecture of Phidget22, and how channels are matched to device features.
-
-    # Specifies the serial number of the device to attach to.
-    # For VINT devices, this is the hub serial number.
-    #
-    # The default is any device.
-    #
-    # ch.setDeviceSerialNumber(<YOUR DEVICE SERIAL NUMBER>) 
-
-    # For VINT devices, this specifies the port the VINT device must be plugged into.
-    #
-    # The default is any port.
-    #
-    # ch.setHubPort(0)
-
-    # Specifies that the channel should only match a VINT hub port.
-    # The only valid channel id is 0.
-    #
-    # The default is 0 (false), meaning VINT hub ports will never match
-    #
-    # ch.setIsHubPortDevice(1)
-
-    # Specifies which channel to attach to.  It is important that the channel of
-    # the device is the same class as the channel that is being opened.
-    #
-    # The default is any channel.
-    #
-    # ch.setChannel(0)
-
-    # In order to attach to a network Phidget, the program must connect to a Phidget22 Network Server.
-    # In a normal environment this can be done automatically by enabling server discovery, which
-    # will cause the client to discovery and connect to available servers.
-    #
-    # To force the channel to only match a network Phidget, set remote to 1.
-    #
-    # Net.enableServerDiscovery(PhidgetServerType.PHIDGETSERVER_DEVICE);
-    # ch.setIsRemote(1)
 
